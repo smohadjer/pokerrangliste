f3b2d2b5d2a06c340522655f17c04ce5a35ff104
Saeid Mohadjer <saeid@fastmail.fm>
2023-03-27 11:11:05 +0200

    updates

--- api/fetch.js
+++ api/fetch.js
@@ -8,7 +8,7 @@ async function run() {
     console.log('openning db...');
     await client.connect();
     const database = client.db('test');
-    const users = database.collection('poker');
+    const users = database.collection('poker2023');
     const data = await users.find({}).sort({ Siege: -1, Spiele: -1, Name: 1 }).toArray();
     return data;
   } catch (e) {
