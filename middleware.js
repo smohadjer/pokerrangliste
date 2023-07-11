29085e46c835f1ba2bd388654d2cbc550724da14
Saeid Mohadjer <saeid@fastmail.fm>
2023-07-11 18:18:08 +0200

    adds authorization for admin page

---
+++ middleware.js
@@ -0,0 +1,30 @@
+// Basic Auth example via Vercel edge middleware and without using a framework
+
+import { next } from '@vercel/edge';
+
+export default function middleware(req) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'admin' && pwd === 'Opengl?1') {
+      next();
+    } else {
+      return new Response('credentials wrong', {
+        status: 401,
+        headers: { 'WWW-Authenticate': 'Basic' },
+      });
+    }
+  } else {
+    return new Response('credentials missing', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic' },
+    });
+  }
+}
+
+export const config = {
+  matcher: '/admin.html',
+};
