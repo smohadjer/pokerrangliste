afa037da9bcce3a17a5c8c08f757b23055616b09
Saeid Mohadjer <saeid@fastmail.fm>
2023-07-25 16:33:05 +0200

    minor

--- middleware.js
+++ middleware.js
@@ -9,6 +9,7 @@ export default function middleware(req) {
     const authValue = basicAuth.split(' ')[1];
     const [user, pwd] = atob(authValue).split(':');
 
+    // we can not import and use dotenv package in edge runtime, but we can use process.env to access environment variables as mentioned here: https://vercel.com/docs/concepts/functions/edge-functions/edge-runtime
     if (user === process.env.admin_username && pwd === process.env.admin_password) {
       next();
     } else {
