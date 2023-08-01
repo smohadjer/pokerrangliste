f2f0bfb17daed2ebb9ecb8b3bd77afe5fd24edbf
Saeid Mohadjer <saeid@fastmail.fm>
2023-08-01 15:13:59 +0200

    allows using local database

--- api/db.js
+++ api/db.js
@@ -2,9 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Replace the uri string with your connection string.
-const uri =
-`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.8qwlizm.mongodb.net/?retryWrites=true&w=majority`;
-
+const protocol = process.env.development === 'true' ? 'mongodb://' : 'mongodb+srv://';
+const uri = protocol + process.env.db_username + ':' + encodeURIComponent(process.env.db_password) + '@' + process.env.db_url;
 
 export default uri;
