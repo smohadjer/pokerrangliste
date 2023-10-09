15b610174206e7edb8d2b77958d92cad55cddaf4
Saeid Mohadjer <saeid@fastmail.fm>
2023-10-09 08:22:13 +0200

    removes insert.js

--- api/tournament.js
+++ api/tournament.js
@@ -1,4 +1,5 @@
 import client from './db.js';
+import { sanitize } from './sanitize.js';
 import { ObjectId } from 'mongodb';
 
 const getTournaments = async (tournaments, req) => {
@@ -15,17 +16,60 @@ const getTournaments = async (tournaments, req) => {
   }
 };
 
+const insertTournament = async (tournaments, req) => {
+  const count = Number(req.body.count);
+  const players = [];
+  const prizes = [];
+  let rebuys = 0;
+
+  for (let i=0; i<count; i++) {
+    console.log(req.body[`players_${i}_name`]);
+    const player = {};
+    const prize = Number(sanitize(req.body[`players_${i}_prize`]));
+    player.name = sanitize(req.body[`players_${i}_name`]).toLowerCase();
+    player.rebuys = Number(sanitize(req.body[`players_${i}_rebuys`]));
+    player.ranking = i+1;
+    players.push(player);
+
+    if (player.rebuys > 0) {
+      rebuys += player.rebuys;
+    }
+
+    if (prize > 0) {
+      prizes.push(prize);
+    }
+  }
+
+  await tournaments.insertOne({
+    season_id: req.body.season_id,
+    date: sanitize(req.body.date),
+    round: sanitize(req.body.round),
+    buyin: sanitize(req.body.buyin),
+    rebuys: rebuys,
+    prizes: prizes,
+    players: players
+  });
+};
+
 export default async (req, res) => {
   try {
     await client.connect();
     const database = client.db('pokerrangliste');
     const tournaments = database.collection('tournaments');
     const seasons = database.collection('seasons');
-    const data = {
-      seasons: await seasons.find().toArray(),
-      tournaments: await getTournaments(tournaments, req)
-    };
-    res.json(data);
+
+    if (req.method === 'GET') {
+      const data = {
+        seasons: await seasons.find().toArray(),
+        tournaments: await getTournaments(tournaments, req)
+      };
+      res.json(data);
+    }
+
+    if (req.method === 'POST') {
+      await insertTournament(tournaments, req);
+      res.status(200).send({ message: "Tournament inserted" });
+    }
   } catch (e) {
     console.error(e);
   } finally {
