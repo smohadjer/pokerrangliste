f5e2a59a930580b6b6d4f597d5361bf100c19e55
Saeid Mohadjer <saeid@fastmail.fm>
2025-01-16 06:52:35 +0100

    fix for season being lost when going to admin home

--- public/ts/controllers/dashboard.ts
+++ public/ts/controllers/dashboard.ts
@@ -1,9 +1,23 @@
 import { store } from '../lib/store';
 
+type TemplateData = {
+    season_id? : string;
+    event_id: string | null;
+    tenant_name: string;
+}
+
 export default (params: URLSearchParams) => {
     const state = store.getState();
-    return {
-        event_id: params.get('event_id'),
-        tenant_name: state.tenant.name
+    let season_id = params.get('season_id') ?? undefined;
+    if (!season_id && state.seasons?.length > 0) {
+        season_id = state.seasons[state.seasons.length - 1]._id;
     }
+
+    const data: TemplateData = {
+        event_id: params.get('event_id'),
+        tenant_name: state.tenant.name,
+        season_id
+    }
+
+    return data;
 };
